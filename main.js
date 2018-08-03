613031fd43bee2f86dbe4e4a3695fbea7ddfb6c5
Ken Patel <kpatelPro@gmail.com>
2018-08-03 22:23:19 +0200

    calculateWinner - part 1
    convert body positions to seconds
    offset and convert aspects to seconds
    rename 'degrees' to 'positions'
    rewrite degree utility functions (now position utility functions)

--- main.js
+++ main.js
@@ -145,19 +145,45 @@ var astralBodies = [
 */
 ];
 
+var aspectAngles = [
+[0,0,0],[18,0,0],[22,30,0],
+[30,0,0],[36,0,0],[40,0,0],[45,0,0],[51,26,0],
+[60,0,0],[72,0,0],[75,0,0],
+[90,0,0],[105,0,0],[108,0,0],
+[120,0,0],[135,0,0],[144,0,0],[150,0,0],[165,0,0],
+[180,0,0],[195,0,0],
+[210,0,0],[225,0,0],[225,0,0],
+[240,0,0],[252,0,0],[255,0,0],
+[270,0,0],[285,0,0],[288,0,0],
+[300,0,0],[308,34,0],[315,0,0],[320,0,0],[324,0,0],
+[330,0,0],[337,30,0],[342,0,0]
+];
+
+var aspectAnglesInSeconds = [];
+
+var kSecondsPerDegree = 60 * 60;
+var kSecondsPer360Degrees = 360 * 60 * 60;
+
+var calculateData = {
+	auxiliaryBodiesAnglesSeconds : {},
+	astralBodiesAnglesSeconds : {},
+	aspectAnglesSecondsAdjusted : [],
+};
+
 function onReady() {
 	zodiacSignsPrecalculate();
   astralBodyPrecalculate();
-  degreeSignInputsPopulate();
+  positionInputsPopulate();
   
   $("#aspectInputs").show();
   $("#aspectResults").hide();
   
   // button handlers
 	$("#random").click(function(){
-		degreeSignInputsRandomize();
+		positionInputsRandomize();
   });
   $("#calculate").click(function(){
+  	calculateWinner();
     $("#aspectInputs").hide();
     $("#aspectResults").show();
   });
@@ -178,15 +204,15 @@ function zodiacSignsPrecalculate() {
 function astralBodyPrecalculate() {
 	var degreeInSeconds = 60 * 60;
 	astralBodies.forEach(function(value, index, array) {
-		var tomorrowSeconds = degreeToSeconds(value.tomorrow);
-		var todaySeconds = degreeToSeconds(value.today);
+		var tomorrowSeconds = positionToSeconds(value.tomorrow);
+		var todaySeconds = positionToSeconds(value.today);
 		var speedSeconds = (tomorrowSeconds - todaySeconds);
-		value.speed = degreeFromSeconds(Math.floor(speedSeconds / degreeInSeconds));
+		value.speed = positionToSeconds(Math.floor(speedSeconds / degreeInSeconds));
 	});
 }
 
-function degreeSignInputsPopulate() {
-  var table = $("#degreeSignInputs");
+function positionInputsPopulate() {
+  var table = $("#positionInputs");
 
   //header
   var tr = $("<tr></tr>");
@@ -200,25 +226,25 @@ function degreeSignInputsPopulate() {
 
   //rows
   auxiliaryBodies.forEach(function(value, index, array) {
-  	var tr = degreeSignInputsCreateRow(value.id, value.name, value.icon);
+  	var tr = positionInputsCreateRow(value.id, value.name, value.icon);
     table.append(tr);
 	});
   astralBodies.forEach(function(value, index, array) {
-  	var tr = degreeSignInputsCreateRow(value.id, value.name, value.icon);
+  	var tr = positionInputsCreateRow(value.id, value.name, value.icon);
     table.append(tr);
 	});
 }
 
-function degreeSignInputsCreateRow(id, name, icon) {	
+function positionInputsCreateRow(id, name, icon) {	
 	var tr = $("<tr></tr>");
 	var tdIcon = $(
 		'<td><img id="icon' + id + '" src="' + icon + '"></img></td>'
 	);
 	var tdName = $("<td>" + name + "</td>");
 	var tdDegree = $('<td></td>');
-	tdDegree.append(degreeInputCreate('Degrees'+id, 30));
-	tdDegree.append(degreeInputCreate('Minutes'+id, 60));
-	tdDegree.append(degreeInputCreate('Seconds'+id, 60));
+	tdDegree.append(offsetInputCreate('Degrees'+id, 30));
+	tdDegree.append(offsetInputCreate('Minutes'+id, 60));
+	tdDegree.append(offsetInputCreate('Seconds'+id, 60));
 	
 	var tdSignSymbol = $('<td id="tdSign' + id + '"></td>');
 	var signSymbol = $('<div id="signSymbol' + id + '">' + zodiacSigns["ARI"].symbolCharacter + "</div>");
@@ -233,8 +259,16 @@ function degreeSignInputsCreateRow(id, name, icon) {
  	return tr;
 }
 
-function degreeInputCreate(id, valueRange) {
-	var $input = $('<input id="degree' + id + '" type="number" min="0" max="' + (valueRange-1) + '" size="3" maxlength="3" required>');
+function positionInputsGet(id) {
+		var sign = $('#sign' + id).val();
+		var degrees = $('#offsetDegrees' + id).val();
+		var minutes = $('#offsetMinutes' + id).val();
+		var seconds = $('#offsetSeconds' + id).val();
+		var position = [sign, degrees, minutes, seconds];
+}
+
+function offsetInputCreate(id, valueRange) {
+	var $input = $('<input id="offset' + id + '" type="number" min="0" max="' + (valueRange-1) + '" size="3" maxlength="3" required>');
 	return $input;
 }
 
@@ -258,64 +292,133 @@ function signInputCreate(id) {
   return div;
 }
 
-function degreeSignInputsRandomize() {
+function positionInputsRandomize() {
 	auxiliaryBodies.forEach(function(value, index, array) {
-		degreeSignInputsRowRandomize(value.id);
+		positionInputsRowRandomize(value.id);
 	});
 	astralBodies.forEach(function(value, index, array) {
-		degreeSignInputsRowRandomize(value.id);
+		positionInputsRowRandomize(value.id);
 	});
 }
 
-function degreeSignInputsRowRandomize(id) {
-		$('#degreeDegrees' + id).val(randomRangeInt(0,30));
-		$('#degreeMinutes' + id).val(randomRangeInt(0,60));
-		$('#degreeSeconds' + id).val(randomRangeInt(0,60));
+function positionInputsRowRandomize(id) {
+		$('#offsetDegrees' + id).val(randomRangeInt(0,30));
+		$('#offsetMinutes' + id).val(randomRangeInt(0,60));
+		$('#offsetSeconds' + id).val(randomRangeInt(0,60));
 		$('#sign' + id).val(randomFromArray(zodiacKeys));		
  		$('#sign' + id).change();
 }
 
-function degreeFromString(input) {
-	var degree = input.split(':');
-	while (degree.length < 3) {
-		degree.unshift(0);
-	}
-	return degree;
+function calculateWinner() {
+	// convert aspect angles into seconds
+	aspectAnglesInSeconds = [];
+	aspectAngles.forEach(function(value, index, array) {
+		var angle = value;
+		var angleInSeconds = positionToSeconds(angle);
+		apectAnglesInSeconds.push(angleInSeconds);
+	});
+
+	// recalculate all data	
+	calculateData = {};
+	// convert auxiliary body positions to seconds
+	calculateData.auxiliaryBodiesAnglesSeconds = {};
+	auxiliaryBodies.forEach(function(value, index, array) {
+			var position = positionInputsGet(value.id);
+			calculateData.auxiliaryBodiesAnglesSeconds[value.id] = positionToSeconds(position);
+	});
+	astralBodies.forEach(function(value, index, array) {
+			var position = positionInputsGet(value.id);
+			calculateData.astralBodiesAnglesSeconds[value.id] = positionToSeconds(position);
+	});
+	
+	// adjust aspects for cusp5 angle
+	var cusp5PositionSeconds = calculateData.auxiliaryBodiesAnglesSeconds["CUSP5"];
+	apectAnglesInSeconds.forEach(function(value, index, array) {
+		var angleInSeconds = value;
+		var adjustedSeconds = positionSecondsWrap(angleInSeconds + cusp5PositionSeconds);
+		calculateData.aspectAnglesSecondsAdjusted.push(adjustedSeconds);
+	});
 }
 
-function degreeFromSeconds(degree) {
-	while (degree.length < 3) {
-		degree.unshift(0);
+function positionFromString(input) {
+	var position = input.split(':');
+	while (position.length < 3) {
+		position.unshift(0);
 	}
-	var seconds = 0;
-	seconds += degree[0];
-	seconds *= 60;
-	seconds += degree[1];
-	seconds *= 60;
-	seconds += degree[2];
+	return position;
+}
+
+function positionToSeconds(position) {
+	
+	// a position must include at least 3 parts
+	while (position.length < 3) {
+		position.unshift(0);
+	}
+		
+	// get components
+	var degree = position[position.length-3];
+	// check if position includes zodiac sign
+	if (position.length > 3) {
+		// if so, add the degree offset for the zodiac sign
+		var sign = position[position.length-4];
+		if (sign in zodiacSigns) {
+			var signAngle = zodiacSigns[sign].angle;
+			degree += signAngle;
+		}
+	}
+	var minutes = position[position.length-2];
+	var seconds = position[position.length-1];
+	
+	// calculate totals
+	var totalSeconds = 0;
+	totalSeconds += degree; 
+	totalSeconds *= 60;
+	totalSeconds += minutes;
+	totalSeconds *= 60;
+	totalSeconds += seconds;
+	
+	// wrap
+	totalSeconds = positionSecondsWrap(totalSeconds);
+	
+	// return
+	return totalSeconds;
+}
+	
+function positionSecondsWrap(seconds) {
+	
+	// make sure it is not negative
+	while (seconds < 0) {
+		seconds = seconds + kSecondsPer360Degrees;
+	}
+	// make sure it does not exceed 360'
+	while (seconds >= kSecondsPer360Degrees) {
+		seconds = seconds - kSecondsPer360Degrees;
+	}
+	
+	// return
 	return seconds;
 }
 
-function degreeToSeconds(seconds) {
-	var degree = [];
-	var remainder;
+function positionFromSeconds(totalSeconds) {
 	
-	remainder = seconds % 60;
-	degree.unshift(remainder);
-	seconds -= remainder;
-	seconds /= 60;
+	// extract seconds
+	var seconds = totalSeconds % 60;
 	
-	remainder = seconds % 60;
-	degree.unshift(remainder);
-	seconds -= remainder;
-	seconds /= 60;
+	// extract minutes	
+	var totalMinutes = (totalSeconds - seconds) / 60;
+	var minutes = totalMinutes % 60;
 	
-	remainder = seconds;
-
-	while (degree.length < 3) {
-		degree.unshift(0);
+	// extract degrees
+	var degrees = (totalMinutes - minutes) / 60;
+	while (degrees < 0) {
+		degrees = degrees + 360;
 	}
-	return degree;
+	while (degrees >= 360) {
+		degrees = degrees - 360;
+	}
+	
+	var position = [degrees, minutes, seconds];
+	return position;
 }
 
 function randomRangeInt(min, max)
